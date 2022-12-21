af9a11a6a6aedd33178bc06e8ee156300a994ff6
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2022-12-21 13:23:55 +0100

    changed routes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -24,10 +24,10 @@ export default function Index() {
               <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
                 {user ? (
                   <Link
-                    to="/notes"
+                    to="/subscriptions"
                     className="flex items-center justify-center rounded-md border border-transparent bg-primary px-4 py-3 text-base font-medium text-light shadow-sm hover:bg-blue-50 hover:text-primary sm:px-8"
                   >
-                    View Notes for {user.email}
+                    View Subscriptions for {user.email}
                   </Link>
                 ) : (
                   <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
