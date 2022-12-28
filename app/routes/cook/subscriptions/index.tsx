63ab4b07c4a8bfabba720ff85910fc3da43e71bc
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2022-12-28 19:49:31 +0100

    removed create link

--- app/routes/cook/subscriptions/index.tsx
+++ app/routes/cook/subscriptions/index.tsx
@@ -3,10 +3,7 @@ import { Link } from "@remix-run/react";
 export default function NoteIndexPage() {
   return (
     <p>
-      No subscription selected. Select a subscription on the left, or{" "}
-      <Link to="new" className="text-blue-500 underline">
-        create a new one.
-      </Link>
+      No subscription selected. Select a subscription on the left.
     </p>
   );
 }
