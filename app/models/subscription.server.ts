0b9492d7272e4657d1b0cac2d06a87fd87a5972f
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2022-12-30 23:47:20 +0100

    removed include

--- app/models/subscription.server.ts
+++ app/models/subscription.server.ts
@@ -1,4 +1,4 @@
-import type { Subscription, User } from "@prisma/client";
+import type { Subscription, SubscriptionMeal, User } from "@prisma/client";
 
 import { prisma } from "~/db.server";
 
@@ -35,17 +35,7 @@ export async function getSubscriptionMeals(id: Subscription["id"]) {
       subscriptionId: id,
     },
     include: {
-      subscriptionMeals: {
-        include: {
-          meal: true,
-        },
-      },
-      user: includeUser && {
-        select: {
-          id: true,
-          email: true,
-        }
-      },
+      meal: true,
     }
   });
 }
@@ -54,6 +44,39 @@ export async function deleteSubscription(id: Subscription["id"]) {
   return prisma.subscription.delete({
     where: {
       id,
+    },
+  });
+}
+
+export async function createSubscription(body: any) {
+  // first subscription,
+  // then subscriptionMeals
+  // then connect subscriptionMeals to subscription
+  const price = body.meals.reduce((acc: number, meal: SubscriptionMeal) => acc + meal.price, 0);
+
+  // change meals field with meal.id and cook with meal.cookId
+  body.meals = body.meals.map((meal: any) => {
+
+    const _meal = meal;
+    _meal.mealId = meal.meal.id;
+    _meal.cookId = meal.meal.cookId;
+    delete _meal.meal;
+
+    return _meal;
+  });
+  
+  
+
+  return prisma.subscription.create({
+    data: {
+      userId: body.user,
+      subscriptionMeals: {
+        create: body.meals,
+      },
+      title: body.title,
+      end: body.end,
+      start: body.startDate,
+      price,
     },
   });
 }
