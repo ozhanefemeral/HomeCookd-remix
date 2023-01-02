5bf1737460f6b80dff53f60e9a695ac3124a7015
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2023-01-02 19:39:52 +0100

    table added

--- app/routes/cook/subscriptions/$day.tsx
+++ app/routes/cook/subscriptions/$day.tsx
@@ -1,11 +1,18 @@
 import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useCatch, useLoaderData } from "@remix-run/react";
+import { Form, useCatch, useLoaderData, useNavigate } from "@remix-run/react";
 import invariant from "tiny-invariant";
-
 import { deleteSubscription } from "~/models/subscription.server";
-import { getSubscriptionMealById, deleteSubscriptionMeal, getSubscriptionMealsByDay, DeliveryDay } from "~/models/subscriptionMeal.server";
+import { getSubscriptionMealById, deleteSubscriptionMeal, getSubscriptionMealsByDay, DeliveryDay, SubscriptionMealWithMeal } from "~/models/subscriptionMeal.server";
 import { requireCookId } from "~/session.server";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  Row,
+  useReactTable,
+} from '@tanstack/react-table'
+import { SubscriptionMeal } from "@prisma/client";
 
 export async function loader({ request, params }: LoaderArgs) {
   const cookId = await requireCookId(request);
@@ -18,7 +25,12 @@ export async function loader({ request, params }: LoaderArgs) {
     throw new Response("Not Found", { status: 404 });
   }
 
-  // sort meals by delivery hour
+  // add 0 to the hour so it sorts correctly if it's a single digit
+  subMeals.forEach((subMeal) => {
+    subMeal.deliveryHour = subMeal.deliveryHour.padStart(2, "0");
+  });
+
+  // sort by delivery hour
   subMeals.sort((a, b) => {
     if (a.deliveryHour < b.deliveryHour) {
       return -1;
@@ -32,28 +44,92 @@ export async function loader({ request, params }: LoaderArgs) {
   return json({ subMeals });
 }
 
+const columnHelper = createColumnHelper<SubscriptionMealWithMeal>()
+const columns = [
+  columnHelper.accessor('meal.title', {
+    cell: info => info.getValue(),
+    header: () => 'Meal',
+  }),
+  columnHelper.accessor('deliveryHour', {
+    cell: info => info.getValue(),
+    header: () => 'Delivery Hour',
+  }),
+  columnHelper.accessor('quantity', {
+    cell: info => info.getValue(),
+    header: () => 'Quantity',
+  }),
+  columnHelper.accessor('meal.price', {
+    // cell with $ sign
+    cell: info => `$${info.getValue()}`,
+    header: () => 'Price',
+  }),
+]
+
 export default function NoteDetailsPage() {
   const data = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
+
+  const table = useReactTable({
+    // WTF DID I DO HERE
+    data: data.subMeals as unknown as SubscriptionMealWithMeal[],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
 
   const count = data.subMeals.length;
 
+  function handleRowClick(row: Row<SubscriptionMealWithMeal>) {
+    const subMeal = row.original;
+    navigate(`/cook/subscriptions/meals/${subMeal.id}`);
+  }
+
   return (
-    <div className="p-4">
+    <div>
       {/* tailwind h3 for count with sentence */}
       {count > 0 ?
         <h3 className="text-2xl font-bold text-center">You have {count} meals for this day 😍</h3> :
         <h3 className="text-2xl font-bold text-center">You have no meals for this day 🙄</h3>}
-      <ul>
-        {/* navlink to subscriotion meals */}
-        {data.subMeals.map((subMeal) => (
-          <li key={subMeal.id}>
-            <a href={`/cook/subscriptions/meals/${subMeal.id}`}>
-              {subMeal.meal.title} - {subMeal.quantity} - {subMeal.deliveryHour}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+      <div className="py-4">
+        <hr />
+      </div>
+      {count > 0 && <div>
+        {/* some subtitle with low opacity and italic to let users they can click row to view */}
+        <h3 className="text-lg text-gray-500 italic pb-2">Click on a row to view meal details</h3>
+        <table className="border-solid border-2">
+          <thead>
+            {table.getHeaderGroups().map(headerGroup => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map(header => (
+                  <th key={header.id}
+                    colSpan={header.colSpan}
+                    style={{ width: header.getSize() }}
+                    className="text-primary"
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map(row => (
+              <tr key={row.id} onClick={() => handleRowClick(row)} className="cursor-pointer hover:text-primary hover:font-bold">
+                {row.getVisibleCells().map(cell => (
+                  <td key={cell.id} className="p-2">
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>}
+    </div >
   );
 }
 
