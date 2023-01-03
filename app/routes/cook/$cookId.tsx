6332a6a71d22db7180ba0bcb7af4015b2ce8d450
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2023-01-03 16:41:14 +0100

    added padding

--- app/routes/cook/$cookId.tsx
+++ app/routes/cook/$cookId.tsx
@@ -1,7 +1,7 @@
 // remix page where cookId is a param and is used in the loader function
 // Path: app\routes\cook\$cookId.tsx
 
-import { Form, useCatch, useLoaderData } from "@remix-run/react";
+import { Form, Outlet, useCatch, useLoaderData } from "@remix-run/react";
 import { json, LoaderArgs } from "@remix-run/server-runtime";
 import { getCookById, getCookMeals } from "~/models/cook.server";
 
@@ -17,9 +17,12 @@ export async function loader({ request, params }: LoaderArgs) {
 export default function CookPage() {
   const data = useLoaderData<typeof loader>();
   return (
-    <div>
+    <div className="p-4">
       {/* h1 as cook name */}
       <h1 className="text-2xl font-bold">{data.cook?.name}</h1>
+      <div className="flex-1">
+        <Outlet />
+      </div>
     </div>
   );
 }
