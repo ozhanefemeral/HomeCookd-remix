6153a0754226d0e57d03459edf899803df084e83
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2023-01-08 19:56:52 +0100

    height fix

--- app/routes/user/subscriptions.tsx
+++ app/routes/user/subscriptions.tsx
@@ -19,7 +19,7 @@ export default function SubscriptionsPage() {
   const user = useUser();
 
   return (
-    <div className="flex h-full min-h-screen flex-col">
+    <div className="flex flex-col">
       <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
         <h1 className="text-3xl font-bold">
           <Link to=".">Subscriptions</Link>
