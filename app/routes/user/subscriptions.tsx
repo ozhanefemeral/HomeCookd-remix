b10f6f7e015cfe5859e456cc3af664950069c609
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2023-01-24 22:35:45 +0100

    moved navbar

--- app/routes/user/subscriptions.tsx
+++ app/routes/user/subscriptions.tsx
@@ -20,20 +20,6 @@ export default function SubscriptionsPage() {
 
   return (
     <div className="flex flex-col">
-      <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
-        <h1 className="text-3xl font-bold">
-          <Link to=".">Subscriptions</Link>
-        </h1>
-        <p>{user.email}</p>
-        <Form action="/logout" method="post">
-          <button
-            type="submit"
-            className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
-          >
-            Logout
-          </button>
-        </Form>
-      </header>
 
       <main className="flex h-full bg-white">
         <div className="h-full w-80 border-r bg-gray-50">
