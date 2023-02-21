d68b1ae92efda7c6d211a12bdc3f6e27580c4c7b
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2023-02-21 03:23:01 +0100

    steps added

--- app/routes/user/onboarding.tsx
+++ app/routes/user/onboarding.tsx
@@ -2,12 +2,16 @@
 
 import { useUserProfile } from "~/utils";
 import { useNavigate } from "@remix-run/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { MealTags } from "@prisma/client";
+
+const ONBBARDING_STEPS = ["tags", "schedule", "payment", "done"];
 
 const ProfileOnboarding = () => {
   const userProfile = useUserProfile();
   const navigate = useNavigate();
-
+  const [appliedTags, setAppliedTags] = useState<MealTags[]>([]);
+  const [step, setStep] = useState(0);
 
   useEffect(() => {
     if (!userProfile) navigate("/login")
@@ -15,7 +19,14 @@ const ProfileOnboarding = () => {
   }, [userProfile]);
 
   return <div>
-    <h1 className="text-2xl font-bold">Onboarding</h1>
+    <h1 className="text-2xl font-bold">Onboarding for {userProfile.name}</h1>
+    {/* tailwind buttons for next and back */}
+    {/* if step is 0, show tags */}
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 disabled:bg-gray-500 px-4 rounded" onClick={() => setStep(step + 1)} disabled={step === 3}>Next</button>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold disabled:bg-gray-500 py-2 px-4 rounded" onClick={() => setStep(step - 1)} disabled={step === 0}>Back</button>
+
+    {/* first, we need to know what food categories they are interested in */}
+    {/* cards of meal tags */}
   </div>;
 }
 
