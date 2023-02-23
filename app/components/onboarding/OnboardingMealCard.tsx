a6343dd8f76b9949352f7e08a0f22ee09a99a50d
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2023-02-24 00:32:52 +0100

    added subscription step

---
+++ app/components/onboarding/OnboardingMealCard.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { MealWithCook } from "~/models/meals.server";
+import { formatMealTag, mapMealTagToEmoji } from "~/utils";
+
+type MealCardProps = {
+  meal: MealWithCook;
+  onClick?: () => void;
+};
+
+export default function OnboardingMealCard({ meal, onClick }: MealCardProps) {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const clickHandler = (e: React.MouseEvent) => {
+    setIsSelected(!isSelected);
+    onClick?.();
+  };
+
+  return (
+    <div className={`rounded-lg p-4 shadow-lg hover:cursor-pointer ${isSelected && "bg-violet-400"}`} onClick={clickHandler}>
+      {/* image on top, meal name and cook name below */}
+      <div className="flex flex-col gap-2">
+        <img src={meal.image} alt={meal.title} className="rounded-lg" />
+        <div className="flex flex-row gap-2 align-bottom">
+          <h3 className="text-lg font-bold">{meal.title}</h3>
+          <span>by {meal.cook.name}</span>
+        </div>
+      </div>
+      {/* meal tags */}
+      <div className="flex flex-row flex-wrap gap-2">
+        {meal.tags.map((tag) => (
+          <span
+            key={tag}
+            className="rounded-full bg-gray-200 px-2 py-1 text-gray-900"
+          >
+            {mapMealTagToEmoji(tag)} {formatMealTag(tag)}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}
--- app/routes/user/onboarding.tsx
+++ app/routes/user/onboarding.tsx
@@ -1,11 +1,14 @@
 // simple div with h1 for now
 
 import { formatMealTag, mapMealTagToEmoji, useUserProfile } from "~/utils";
-import { useNavigate } from "@remix-run/react";
+import { useFetcher, useNavigate } from "@remix-run/react";
 import { useEffect, useState } from "react";
-import { MealTags } from "@prisma/client";
+import { Meal, MealTags } from "@prisma/client";
+import { json, LoaderArgs } from "@remix-run/server-runtime";
+import { getMealByTags, MealWithCook } from "~/models/meals.server";
+import OnboardingMealCard from "~/components/onboarding/OnboardingMealCard";
 
-const ONBBARDING_STEPS = ["tags", "schedule", "payment", "done"];
+const ONBBARDING_STEPS = ["tags", "subscription", "payment", "done"];
 
 type TagsStepProps = {
   interestedTags: MealTags[];
@@ -14,74 +17,164 @@ type TagsStepProps = {
   setDislikedTags: (tags: MealTags[]) => void;
 };
 
-function TagsStep({ interestedTags, setInterestedTags, dislikedTags, setDislikedTags }: TagsStepProps): JSX.Element {
+type SubscriptionStepProps = {
+  meals: MealWithCook[];
+  nextStep: () => void;
+};
 
+function TagsStep({
+  interestedTags,
+  setInterestedTags,
+  dislikedTags,
+  setDislikedTags,
+}: TagsStepProps): JSX.Element {
   // TODO
   // DISLIKES AND INTERESTS CAN'T INCLUDE THE SAME TAG
-  return <>
-    <h2 className="text-xl font-bold">What are you interested in?</h2>
-    <div className="flex flex-row gap-4 flex-wrap">
-      {Object.values(MealTags).map((tag) => (
-        <button
-          key={tag}
-          // bg primary and white text if applied, light gray bg and black text if not applied
-          className={`rounded px-4 py-2 ${interestedTags.includes(tag) ? "bg-primary text-white" : "bg-gray-200 text-gray-900"
+  return (
+    <>
+      <h2 className="text-xl font-bold">What are you interested in?</h2>
+      <div className="flex flex-row flex-wrap gap-4">
+        {Object.values(MealTags).map((tag) => (
+          <button
+            key={tag}
+            // bg primary and white text if applied, light gray bg and black text if not applied
+            className={`rounded px-4 py-2 ${
+              interestedTags.includes(tag)
+                ? "bg-primary text-white"
+                : "bg-gray-200 text-gray-900"
             }`}
-          onClick={() => {
-            if (interestedTags.includes(tag)) {
-              setInterestedTags(interestedTags.filter((t) => t !== tag));
-            } else {
-              setInterestedTags([...interestedTags, tag]);
-            }
-          }}
-        >
-          {mapMealTagToEmoji(tag)} {formatMealTag(tag)} {interestedTags.includes(tag) && <span className="text-white">✓</span>}
-        </button>
-      ))}
-      {/* clear all */}
-      {interestedTags.length > 0 && (
-        <button
-          className="rounded px-4 py-2 bg-gray-200 text-gray-900"
-          onClick={() => {
-            setInterestedTags([]);
-          }}
-        >
-          Remove all
-        </button>
-      )}
-    </div>
-    <h2 className="text-xl font-bold">Your allergies/dislikes?</h2>
-    <div className="flex flex-row gap-4 flex-wrap">
-      {Object.values(MealTags).map((tag) => (
-        <button
-          key={tag}
-          // bg primary and white text if applied, light gray bg and black text if not applied
-          className={`rounded px-4 py-2 ${dislikedTags.includes(tag) ? "bg-primary text-white" : "bg-gray-200 text-gray-900"
+            onClick={() => {
+              if (interestedTags.includes(tag)) {
+                setInterestedTags(interestedTags.filter((t) => t !== tag));
+              } else {
+                setInterestedTags([...interestedTags, tag]);
+              }
+            }}
+          >
+            {mapMealTagToEmoji(tag)} {formatMealTag(tag)}{" "}
+            {interestedTags.includes(tag) && (
+              <span className="text-white">✓</span>
+            )}
+          </button>
+        ))}
+        {/* clear all */}
+        {interestedTags.length > 0 && (
+          <button
+            className="rounded bg-gray-200 px-4 py-2 text-gray-900"
+            onClick={() => {
+              setInterestedTags([]);
+            }}
+          >
+            Remove all
+          </button>
+        )}
+      </div>
+      <h2 className="text-xl font-bold">Your allergies/dislikes?</h2>
+      <div className="flex flex-row flex-wrap gap-4">
+        {Object.values(MealTags).map((tag) => (
+          <button
+            key={tag}
+            // bg primary and white text if applied, light gray bg and black text if not applied
+            className={`rounded px-4 py-2 ${
+              dislikedTags.includes(tag)
+                ? "bg-primary text-white"
+                : "bg-gray-200 text-gray-900"
             }`}
-          onClick={() => {
-            if (dislikedTags.includes(tag)) {
-              setDislikedTags(dislikedTags.filter((t) => t !== tag));
-            } else {
-              setDislikedTags([...dislikedTags, tag]);
-            }
+            onClick={() => {
+              if (dislikedTags.includes(tag)) {
+                setDislikedTags(dislikedTags.filter((t) => t !== tag));
+              } else {
+                setDislikedTags([...dislikedTags, tag]);
+              }
+            }}
+          >
+            {mapMealTagToEmoji(tag)} {formatMealTag(tag)}{" "}
+            {dislikedTags.includes(tag) && (
+              <span className="text-white">✓</span>
+            )}
+          </button>
+        ))}
+        {/* clear all */}
+        {dislikedTags.length > 0 && (
+          <button
+            className="rounded bg-gray-200 px-4 py-2 text-gray-900"
+            onClick={() => {
+              setDislikedTags([]);
+            }}
+          >
+            Remove all
+          </button>
+        )}
+      </div>
+    </>
+  );
+}
+
+function SubscriptionStep({ meals, nextStep }: SubscriptionStepProps) {
+  const [selectedMeals, setSelectedMeals] = useState<MealWithCook[]>([]);
+
+  const canGoNext = selectedMeals.length > 0;
+
+  function handleClick() {
+    // TODO
+    // POST TO API
+    if (canGoNext) nextStep();
+  }
+
+  return (
+    <div>
+      <h2 className="text-xl font-bold">Lets subscribe you to a meal!</h2>
+      {/* meal grids of 3 */}
+      {/* tailwind grid */}
+      <div className="grid grid-cols-3 gap-4 xl:px-[10rem]">
+        {meals.map((meal) => (
+          <OnboardingMealCard
+            key={meal.id}
+            meal={meal}
+            onClick={() => {
+              if (selectedMeals.includes(meal)) {
+                setSelectedMeals(selectedMeals.filter((m) => m.id !== meal.id));
+              } else {
+                setSelectedMeals([...selectedMeals, meal]);
+              }
+            }}
+          />
+        ))}
+        <div
+          style={{
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            // some colored gradient with bg-violet-400
+            background:
+              "linear-gradient(90deg, rgb(167 139 250) 0%, rgb(255 187 255) 100%)",
           }}
+          className={`rounded-lg text-2xl font-bold text-white opacity-40 shadow-lg duration-150 ${
+            canGoNext
+              ? "cursor-pointer hover:opacity-100"
+              : "cursor-not-allowed"
+          }`}
+          onClick={handleClick}
         >
-          {mapMealTagToEmoji(tag)} {formatMealTag(tag)} {dislikedTags.includes(tag) && <span className="text-white">✓</span>}
-        </button>
-      ))}
-      {/* clear all */}
-      {dislikedTags.length > 0 && (
-        <button
-          className="rounded px-4 py-2 bg-gray-200 text-gray-900"
-          onClick={() => {
-            setDislikedTags([]);
-          }}
-        >
-          Remove all
-        </button>
-      )}
+          Continue
+        </div>
+      </div>
     </div>
-  </>
+  );
+}
+
+export async function action({ request, context }: LoaderArgs) {
+  const formData = await request.formData();
+  const interestedTags = JSON.parse(
+    formData.get("interestedTags") as string
+  ) as MealTags[];
+  const dislikedTags = JSON.parse(
+    formData.get("dislikedTags") as string
+  ) as MealTags[];
+  const meals = await getMealByTags(interestedTags, dislikedTags);
+  return json({ meals });
 }
 
 const ProfileOnboarding = () => {
@@ -90,24 +183,65 @@ const ProfileOnboarding = () => {
   const [interestedTags, setInterestedTags] = useState<MealTags[]>([]);
   const [dislikedTags, setDislikedTags] = useState<MealTags[]>([]);
   const [step, setStep] = useState(0);
+  const fetcher = useFetcher();
 
   useEffect(() => {
-    if (!userProfile) navigate("/login")
-    else if (userProfile.onboardingCompleted) navigate("/user/profile")
+    if (!userProfile) navigate("/login");
+    else if (userProfile.onboardingCompleted) navigate("/user/profile");
   }, [userProfile]);
 
-  return <div>
-    <h1 className="text-2xl font-bold">Onboarding for {userProfile.name}</h1>
-    {/* tailwind buttons for next and back */}
-    {/* if step is 0, show tags */}
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 disabled:bg-gray-500 px-4 rounded" onClick={() => setStep(step + 1)} disabled={step === 3}>Next</button>
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold disabled:bg-gray-500 py-2 px-4 rounded" onClick={() => setStep(step - 1)} disabled={step === 0}>Back</button>
+  useEffect(() => {
+    if (step === 1) {
+      // fetch recommended meals
+      const recommendedMealForm = new FormData();
+      recommendedMealForm.append(
+        "interestedTags",
+        JSON.stringify(interestedTags)
+      );
+      recommendedMealForm.append("dislikedTags", JSON.stringify(dislikedTags));
+      fetcher.submit(recommendedMealForm, { method: "post" });
+    }
+  }, [step]);
 
-    {step === 0 && <TagsStep interestedTags={interestedTags} setInterestedTags={setInterestedTags} dislikedTags={dislikedTags} setDislikedTags={setDislikedTags} />}
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Onboarding for {userProfile.name}</h1>
+      {/* tailwind buttons for next and back */}
+      {/* if step is 0, show tags */}
+      <button
+        className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700 disabled:bg-gray-500"
+        onClick={() => setStep(step + 1)}
+        disabled={step === 3}
+      >
+        Next
+      </button>
+      <button
+        className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700 disabled:bg-gray-500"
+        onClick={() => setStep(step - 1)}
+        disabled={step === 0}
+      >
+        Back
+      </button>
 
-    {/* first, we need to know what food categories they are interested in */}
-    {/* cards of meal tags */}
-  </div>;
-}
+      {step === 0 && (
+        <TagsStep
+          interestedTags={interestedTags}
+          setInterestedTags={setInterestedTags}
+          dislikedTags={dislikedTags}
+          setDislikedTags={setDislikedTags}
+        />
+      )}
+      {step === 1 && (
+        <SubscriptionStep
+          meals={fetcher.data?.meals || []}
+          nextStep={() => setStep(step + 1)}
+        />
+      )}
 
-export default ProfileOnboarding
+      {/* first, we need to know what food categories they are interested in */}
+      {/* cards of meal tags */}
+    </div>
+  );
+};
+
+export default ProfileOnboarding;
