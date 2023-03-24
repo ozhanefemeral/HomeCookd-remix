30ec13e6ee3628e48f2aab6b07f5070cacd751cc
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2023-03-24 20:11:09 +0100

    add nullable

--- app/components/Subscriptions/FeaturedSubscriptionHomePage.tsx
+++ app/components/Subscriptions/FeaturedSubscriptionHomePage.tsx
@@ -5,12 +5,16 @@ import MealTagTooltip from "../MealTagTooltip";
 import CardTags from "./CardTags";
 import ReserveCount from "./ReserveCount";
 
+type Props = {
+  subscription?: SubscriptionWithCookAndMeal;
+  handleSubscribeClick: (subscription: SubscriptionWithCookAndMeal) => void;
+};
+
 const FeaturedSubscriptionHomePage = ({
   subscription,
-}: {
-  subscription: SubscriptionWithCookAndMeal;
-}) => {
-  const { meal, cook } = subscription;
+  handleSubscribeClick,
+}: Props) => {
+  const { meal, cook } = subscription! || {};
 
   return (
     //card with max width of 360px
@@ -18,14 +22,14 @@ const FeaturedSubscriptionHomePage = ({
       {/* meal image */}
       <div className="relative flex h-52 w-full flex-col items-center justify-center">
         <img
-          src={meal.image || "https://dummyimage.com/256x256"}
+          src={meal!.image || "https://dummyimage.com/256x256"}
           alt="meal"
           className="h-full w-full object-cover"
         />
         <div className="absolute -bottom-10 right-6 mt-10 h-20 w-20">
           <img
             className="h-full w-full rounded-full ring-4 ring-white"
-            src={cook.avatar || "https://dummyimage.com/256x256"}
+            src={cook!.avatar || "https://dummyimage.com/256x256"}
             alt="avatar"
           />
           <div className="slate-900 mt-5 flex items-center justify-center gap-2 text-lg font-bold">
@@ -44,7 +48,7 @@ const FeaturedSubscriptionHomePage = ({
                 stroke-linejoin="round"
               />
             </svg>
-            {subscription.orderHours[1]}
+            {subscription!.orderHours[1]}
           </div>
         </div>
 
@@ -53,15 +57,18 @@ const FeaturedSubscriptionHomePage = ({
       </div>
 
       <ReserveCount
-        reserveCount={subscription.reserveCount}
-        limit={subscription.limit}
+        reserveCount={subscription!.reserveCount}
+        limit={subscription!.limit}
       />
 
       <div className="flex w-full flex-col p-6">
-        <h1 className="text-2xl font-bold">{subscription.title}</h1>
-        <h3 className="mt-2 mb-8 text-sm">{cook.name}</h3>
+        <h1 className="text-2xl font-bold">{subscription!.title}</h1>
+        <h3 className="mt-2 mb-8 text-sm">{cook!.name}</h3>
         {/* green 700, full width, rounded lg button "Sipariş Ver" */}
-        <button className="w-full rounded-lg bg-green-700 py-2 text-white">
+        <button
+          className="w-full rounded-lg bg-green-700 py-2 text-white"
+          onClick={() => handleSubscribeClick(subscription!)}
+        >
           Sipariş Ver
         </button>
       </div>
