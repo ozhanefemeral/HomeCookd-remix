9fb719c7c15f62b160089992c6060fe3bef96d54
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2023-04-02 22:47:24 +0200

    add primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#F97316",
-        "primary-dark": "#1F0078",
+        'primary': {
+          '50': '#fff4ed',
+          '100': '#ffe6d5',
+          '200': '#feccaa',
+          '300': '#fdac74',
+          '400': '#fb8a3c',
+          '500': '#f97316',
+          '600': '#ea670c',
+          '700': '#c2570c',
+          '800': '#9a4a12',
+          '900': '#7c3d12',
+          '950': '#432007',
+          "DEFAULT": "#f97316",
+        },
         light: "#F5F5F5",
         dark: "#1A1A1A",
       }
