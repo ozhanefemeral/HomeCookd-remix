04102e35b23ddb89b1e2fcd9d360f89c332c09a8
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2023-04-28 23:52:32 +0200

    delete

--- app/routes/cook/subscriptions/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "@remix-run/react";
-
-export default function NoteIndexPage() {
-  return (
-    <p>
-      No subscription selected. Select a subscription on the left.
-    </p>
-  );
-}
