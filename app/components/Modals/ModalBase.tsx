b2d8f4c0d475bd5e0ec64be988d20b9224df9777
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2023-06-08 15:47:54 +0200

    remove comments

--- app/components/Modals/ModalBase.tsx
+++ app/components/Modals/ModalBase.tsx
@@ -20,7 +20,6 @@ export function ModalBase({
 }: ModalBaseProps) {
   return (
     <>
-      {/* TODO switch to radixUI with tailwind */}
       <Dialog.Root open={open}>
         <Dialog.Portal>
           <Dialog.Overlay className="fixed inset-0 bg-blackA9 data-[state=open]:animate-overlayShow" />
@@ -45,42 +44,6 @@ export function ModalBase({
           </Dialog.Content>
         </Dialog.Portal>
       </Dialog.Root>
-      {/* <div className="fixed inset-0 bg-black opacity-80" />
-      <div className="fixed inset-0 z-10 flex items-center justify-center">
-        <div className="xs:w-full relative flex flex-col overflow-hidden rounded-xl border border-gray-200 bg-white p-8 shadow-sm sm:w-full md:w-1/3">
-          {showCloseButton && (
-            <div className="absolute top-0 right-0 z-10 p-4">
-              <button
-                type="button"
-                className="z-10 rounded-full bg-gray-200 p-2 text-gray-600 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                onClick={() => setOpen(false)}
-              >
-                <svg
-                  className="h-3 w-3"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-          )}
-          {title && (
-            <h2 className="mb-4 text-center text-2xl font-bold text-primary">
-              {title}
-            </h2>
-          )}
-          {children}
-        </div>
-      </div> */}
     </>
   );
 }
