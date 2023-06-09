9d1b4812f25880047787eecac95348082086d9bc
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2023-06-09 14:59:17 +0200

    fix conditions

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -26,24 +26,24 @@ export function Button({
       className={clsx(
         "flex items-center justify-center rounded-md font-medium text-white",
         {
-          "bg-primary": variant === "primary",
-          "bg-transparent": variant !== "primary",
           "bg-gray-300": disabled && variant === "primary",
+          "bg-primary": !disabled && variant === "primary",
+          "bg-transparent": variant !== "primary",
         },
         {
           underline: variant === "tertiary",
         },
         {
-          "!text-primary": variant !== "primary",
           "text-gray-300": disabled && variant !== "primary",
+          "text-primary": !disabled && variant !== "primary",
         },
         {
-          "border-2 border-primary": variant !== "tertiary",
           "border-2 border-gray-300": disabled && variant !== "tertiary",
+          "border-2 border-primary": !disabled && variant !== "tertiary"
         },
         {
           "p-0": variant === "tertiary",
-          "px-2 py-1 box-border": variant !== "tertiary",
+          "box-border px-2 py-1": variant !== "tertiary",
         },
         {
           "cursor-not-allowed": disabled,
