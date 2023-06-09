81df557c99440b4c6d93e290e2c40f160c4c4ecb
Ozhan Efe Meral <ozhanefemeral@gmail.com>
2023-06-09 22:00:43 +0200

    change cook to cookProfile

--- app/models/cook.server.ts
+++ app/models/cook.server.ts
@@ -1,48 +1,10 @@
-import type { Cook } from "@prisma/client";
+import type { CookProfile, User } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
 import { prisma } from "~/db.server";
 
-export type { Cook } from "@prisma/client";
+export type { CookProfile } from "@prisma/client";
 
-export async function getCookById(id: Cook["id"]) {
-  return prisma.cook.findUnique({ where: { id } });
-}
-
-export async function getCookMeals(username: Cook["username"]) {
-  return prisma.cook.findUnique({ where: { username } }).meals({
-    include: {
-      cook: true,
-    },
-  });
-}
-
-export async function getCookByUsername(username: Cook["username"]) {
-  return prisma.cook.findUnique({ where: { username } });
-}
-
-export async function verifyLogin(
-  email: Cook["email"],
-  password: string
-) {
-
-  const cookWithPassword = await prisma.cook.findUnique({
-    where: { email },
-  });
-
-  if (!cookWithPassword || !cookWithPassword.password) {
-    return null;
-  }
-
-  const isValid = await bcrypt.compare(
-    password,
-    cookWithPassword.password
-  );
-
-  if (!isValid) {
-    return null;
-  }
-
-  const { password: _password, ...cookWithoutPassword } = cookWithPassword;
-  return cookWithoutPassword;
+export async function getCookProfileById(id: User["id"]) {
+  return prisma.cookProfile.findUnique({ where: { userId: id } });
 }
